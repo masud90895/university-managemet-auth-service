359f872be22108a581682a7f712219403832863d
masud90895 <masudhossainmbs129@gmail.com>
2023-05-28 22:15:35 +0600

    project setup added

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const express: any = require("express");
+const app: any = express();
+const port: number = 5000;
+const cors: any = require("cors");
+
+app.use(cors());
+
+app.get("/", (req: any, res: any) => {
+  // Send a response to the browser
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  // Log a message to the terminal
+  console.log(`Example app listening on port ${port}`);
+});
+
+// create a function witch connect local computer on mongoose and create a database
